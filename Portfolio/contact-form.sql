7847e266d9474bd7ddb86ca8dc90851033869ebb
Sundar Maity <189231657+sundarmaity321@users.noreply.github.com>
2025-11-03 04:51:23 +0530

    Add files via upload
    Signed-off-by: Sundar Maity <189231657+sundarmaity321@users.noreply.github.com>

---
+++ Portfolio/contact-form.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 02, 2025 at 06:57 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `get-in-touch`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact-form`
+--
+
+CREATE TABLE `contact-form` (
+  `id` int(255) NOT NULL,
+  `name` char(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `phone` int(25) NOT NULL,
+  `massage` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact-form`
+--
+ALTER TABLE `contact-form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact-form`
+--
+ALTER TABLE `contact-form`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
